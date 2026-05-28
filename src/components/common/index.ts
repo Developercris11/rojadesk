6cac78d0946e69ac1b95bf80cfdc82d1ee72c8e9
Developercris11 <developercris11@gmail.com>
2026-05-28 08:44:25 -0500

    Tese

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,3 @@
-// Re-export common components
-export { Sidebar } from "./sidebar";
-export { ThemeProvider } from "./theme-provider";
-export { ThemeToggle } from "./theme-toggle";
+export { Sidebar } from "../sidebar";
+export { ThemeProvider } from "../theme-provider";
+export { ThemeToggle } from "../theme-toggle";
